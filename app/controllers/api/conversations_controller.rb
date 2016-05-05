f55d22889a1172c9b3d7d4b9401dd212c35a2019
anup-segu <anup.segu@gmail.com>
2016-05-05 14:49:33 -0700

    Update conversation controller create method

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -15,20 +15,20 @@ class Api::ConversationsController < ApplicationController
   def create
     @conversation = Conversation.new(conversation_params)
 
-    # if current_patient
-    #   @conversation.author_id = current_patient.id
-    #   @conversation.author_type = "Patient"
-    # elsif 
-
     if current_doctor
       @conversation.author_id = current_doctor.id
       @conversation.author_type = "Doctor"
     end
 
     if @conversation.save
-      # @parent_conversation = @conversation.parent_conversation
-      @inbox = current_user.received_threads
-      @outbox = current_user.authored_threads
+      if (current_user)
+        user = current_user
+      else
+        user = Patient.find(@conversation.author_id)
+      end
+      
+      @inbox = user.received_threads
+      @outbox = user.authored_threads
       render "api/conversations/index"
     else
       @errors = @conversation.errors.full_messages
