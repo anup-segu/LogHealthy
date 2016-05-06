94aab59cb5620174385ebfc1f1ff0ede4a7fb64b
anup-segu <anup.segu@gmail.com>
2016-05-06 12:02:21 -0700

    Fix error message on duplicate log

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,7 +3,9 @@ class Log < ActiveRecord::Base
   validates :meal_taken?, inclusion: { in: [true, false] }
   validates :meal_type, inclusion: { in: ["breakfast", "lunch", "dinner"] }
 
-  validates :patient_id, uniqueness: { scope: [:meal_type, :date] }
+  validates :patient_id,
+    uniqueness: { scope: [:meal_type, :date], message: ["Log already exists for this meal"] }
+
 
   belongs_to :patient
 end
